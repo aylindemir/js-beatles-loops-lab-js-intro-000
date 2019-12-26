2bf6b552bcbd73f7a913157cb17928906070d734
Aylin Demir <aylin.de@outlook.com>
2019-12-26 23:35:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function johnLennonFacts(facts){
   }
   return facts
 }
+
+function iLoveTheBeatles(number){
+  var variable =[];
+  do{
+    variable.push("I love the Beatles!")
+    number++;
+  }while(number<15)
+}
