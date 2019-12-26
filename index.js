a3ae1241c1dd3242b7f56bdda68bb9297626dc12
Aylin Demir <aylin.de@outlook.com>
2019-12-26 23:33:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function theBeatlesPlay(musicians,instruments){
 function johnLennonFacts(facts){
   for(let i=0;i<facts.length;i++){
     facts[i]=facts[i].concat('!!!')
-
   }
-  // return facts
+  return facts
 }
