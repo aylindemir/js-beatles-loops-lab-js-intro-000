4dc3ab210ae32bcb4e55d5afa8c1e69470af1472
Aylin Demir <aylin.de@outlook.com>
2019-12-26 23:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   var variable =[];
   for(let i=0;i<musicians.length;i++ ){
+    var concanated = musicians[i]+" "+instruments[i]
     variable.push(musicians[i]+instruments[i])
   }
   return variable
